4a3d04236ca8f56dbb9a9977ca40adde69586089
basavaraj-f1 <basavaraj868488@gmail.com>
2026-03-03 21:19:01 +0530

    updated

--- patterns/cent_tri.cpp
+++ patterns/cent_tri.cpp
@@ -1,19 +1,21 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n =5;
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
 
     for(int i=0;i<n;i++){
         for(int j=0;j<n-i;j++){
-            cout<<"*";
+            cout<<"#";
         }
-        for (int j=0;j<2*i;j++){
-            cout<<" ";
+        for(int j=0;j<i*2+1;j++){
+            cout<<"*";
         }
         for(int j=0;j<n-i;j++){
-            cout<<"*";
+            cout<<"%";
         }
         cout<<endl;
     }
-        return 0;
+    return 0;
 }
